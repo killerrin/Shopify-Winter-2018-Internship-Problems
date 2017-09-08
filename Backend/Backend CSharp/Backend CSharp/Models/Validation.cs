c8e100c7fa89d072cfcae978d1aebb4ba502f0cf
Andrew Godfroy <andrew.godfroy@killerrin.com>
2017-09-08 09:11:25 -0400

    Begun work on C# backend

---
+++ Backend/Backend CSharp/Backend CSharp/Models/Validation.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend_CSharp.Models
+{
+    public class Validation
+    {
+        public string Name { get; set; }
+        public ValidationConstraints Constraint { get; set; }
+    }
+
+    public class ValidationConstraints
+    {
+        public bool? Required { get; set; }
+        public string Type { get; set; }
+        public Length Length { get; set; }
+    }
+
+    public class Length
+    {
+        public int? Min { get; set; }
+        public int? Max { get; set; }
+    }
+}
